6479d7a31f60d0e90c5cd7e3e9cfdae541972b7e
andrea.schiavoi <andrea.schiavoi@omnys.com>
2024-08-05 07:46:13 +0200

    first commit

---
+++ src/main/java/com/backlog/backlog_games/BacklogGamesApplication.java
@@ -0,0 +1,13 @@
+package com.backlog.backlog_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacklogGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backlog/backlog_games/BacklogGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backlog.backlog_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklogGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
